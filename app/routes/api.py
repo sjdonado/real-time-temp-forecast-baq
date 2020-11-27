14da53ef6ddeac6a7f03bbb4759074c74b646420
Juan Rodriguez <jsrd98@gmail.com>
2020-11-27 05:34:03 +0000

    chore: update model

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect
 
 from .. import app
 from ..services.get_real_time_obs import run
@@ -6,7 +6,11 @@ from ..services.get_real_time_obs import run
 api_bp = Blueprint('api_bp', __name__)
 
 @api_bp.route('/fetch')
+def fetch():
+  status = run()
+  return jsonify({ 'status': 'sent' if status else 'already in process' })
+
+
+@api_bp.route('/')
 def index():
-  sent = run()
-  
-  return jsonify({ 'status': 'sent' if sent else 'already in process' })
+  return redirect('/dashboard', code=302)
